36f077585e319005edb2b45b7185a3826d4c55dd
kashif.hussain <kashiemails@gmail.com>
2022-03-25 16:13:17 +0500

    Is admin Middleware implemnted

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\IsAdmin;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    //
+})->middleware('isAdmin');
+
+
+// Route::get('/', function () {
+//     $user = Auth::user();
+    
+//     //dd($user);
+
+//     if($user->isAadmin){
+//         echo 'this is admin';
+//     }
+// });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/admin/user/role',['midlleware'=>['role','auth','web'], function(){
+    return 'middleware role';
+
+}]);
